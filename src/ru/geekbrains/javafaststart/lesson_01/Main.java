76bcef6471fce983c809aaa8ed8c46fb7e3a8c3b
Nikolay Shcherbina <Nikolai.V.Shcherbina@gmail.com>
2020-04-25 12:01:19 +0300

    lesson_01 Я буду изучать Java!

--- src/ru/geekbrains/javafaststart/lesson_01/Main.java
+++ src/ru/geekbrains/javafaststart/lesson_01/Main.java
@@ -2,6 +2,6 @@ package ru.geekbrains.javafaststart.lesson_01;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Я буду изучать Java!");
     }
 }
