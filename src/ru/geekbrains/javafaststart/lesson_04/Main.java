0c858e2660ea275bd65d94b13fb48fb7a11bb7c1
Nikolai Shcherbina <Nikolai.V.Shcherbina@gmail.com>
2020-04-29 17:46:54 +0300

    Lesson_04 переместили в метод playLevel

---
+++ src/ru/geekbrains/javafaststart/lesson_04/Main.java
@@ -0,0 +1,37 @@
+package ru.geekbrains.javafaststart.lesson_04;
+
+import java.util.Scanner;
+
+/**
+ * @author Nikolai Shcherbina
+ */
+
+public class Main {
+   private static Scanner scanner = new Scanner(System.in);
+    private static int range = scanner.nextInt();
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Ваша задача угадать число.");
+        System.out.println("Введите число, которое будет максимальное в диапазоне.");
+
+        int number = (int) (Math.random() * range);
+
+        scanner.close();
+    }
+    private static void playLevel(int range, int number){
+        while (true) {
+            System.out.println("Угадайте число от 0 до " + range);
+            int input_number = scanner.nextInt();
+            if (input_number == number) {
+                System.out.println("Вы угадали!");
+                break;
+            } else if (input_number > number) {
+                System.out.println("Загаданное число меньше");
+            } else {
+                System.out.println("Загаданное число больше");
+            }
+        }
+    }
+}
