1e2beb042dad9fefa3bea2e626410556e41cca24
Nikolai Shcherbina <Nikolai.V.Shcherbina@gmail.com>
2020-05-02 14:28:44 +0300

    Lesson_05 3-х уровненвая игра угадай число

--- src/ru/geekbrains/javafaststart/lesson_05/Main.java
+++ src/ru/geekbrains/javafaststart/lesson_05/Main.java
@@ -1,14 +1,35 @@
 package ru.geekbrains.javafaststart.lesson_05;
 
+import java.util.Scanner;
+
 /**
  * @author Nikolai Shcherbina
  */
 
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+        System.out.println("Ваша задача угадать число.");
+        for (int i = 10; i <= 30; i += 10) playLevel(i);
+        System.out.println("Вы выиграли!!!");
+        scanner.close();
+    }
 
-
-
+    private static void playLevel(int range) {
+        int number = (int) (Math.random() * range);
+        while (true) {
+            System.out.println("Угадайте число от 0 до " + range);
+            int input_number = scanner.nextInt();
+            if (input_number == number) {
+                System.out.println("Вы угадали.");
+                break;
+            } else if (input_number > number) {
+                System.out.println("Загаданное число меньше");
+            } else {
+                System.out.println("Загаданное число больше");
+            }
+        }
     }
 }
